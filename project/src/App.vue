b2401c76942036ee3bf234e89107fa79ce1ca558
apachqq <work.tsyganov@mail.ru>
2022-04-16 17:06:55 +0300

    'module2_task1_point_3'

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+<!--    <router-link to="/">Home</router-link>-->
+<!--    |-->
+<!--    <router-link to="/about">About</router-link>-->
     <Main></Main>
   </div>
-  <router-view/>
+<!--  <router-view/>-->
 </template>
 
 <script>
