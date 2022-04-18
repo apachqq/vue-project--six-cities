3d6a4fbc343a11b1b8646340d9303e170e2d7909
apachqq <work.tsyganov@mail.ru>
2022-04-18 20:39:04 +0300

    Hidden_path_route

---
+++ project/src/pages/PageNotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="text">The page you are looking for can't be found.</span>
+  <br>
+  <span class="number">404</span>
+  <router-link class="link" to="/">Main</router-link>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  * {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .text {
+    font-size: 3rem;
+    padding-top: 9rem;
+  }
+
+  .number {
+    font-size: 5rem;
+    padding-top: 8rem;
+  }
+
+  .link {
+    font-size: 2rem;
+    padding-top: 8rem;
+  }
+</style>
